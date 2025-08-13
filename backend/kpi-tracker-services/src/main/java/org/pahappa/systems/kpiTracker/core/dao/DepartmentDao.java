4936c7f8cc4ce4e7c44d6a8ebe3efe5d3b8acf7a
genericodex <152326643+genericodex@users.noreply.github.com>
2025-08-13 15:13:10 +0300

    Initial DAOs, services and views

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/dao/DepartmentDao.java
@@ -0,0 +1,7 @@
+package org.pahappa.systems.kpiTracker.core.dao;
+
+import org.pahappa.systems.kpiTracker.models.Department;
+import org.sers.webutils.server.core.dao.BaseDao;
+
+public interface DepartmentDao extends BaseDao<Department> {
+}
---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/impl/DepartmentServiceImpl.java
@@ -0,0 +1,52 @@
+package org.pahappa.systems.kpiTracker.core.services.impl;
+
+import com.googlecode.genericdao.search.Search;
+import org.pahappa.systems.kpiTracker.core.dao.DepartmentDao;
+import org.pahappa.systems.kpiTracker.core.services.DepartmentService;
+import org.pahappa.systems.kpiTracker.core.services.impl.base.GenericServiceImpl;
+import org.pahappa.systems.kpiTracker.models.Department;
+import org.pahappa.systems.kpiTracker.utils.Validate;
+import org.sers.webutils.model.exception.OperationFailedException;
+import org.sers.webutils.model.exception.ValidationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("departmentService")
+@Transactional
+public class DepartmentServiceImpl extends GenericServiceImpl<Department> implements DepartmentService {
+
+    @Autowired
+    private DepartmentDao departmentDao;
+
+
+//    @Override
+    public boolean isDeletable(Department instance) throws OperationFailedException {
+        // To be implemented: Check if department has users or teams.
+        // For now, we allow deletion.
+        // Example check:
+        // Search userSearch = new Search(User.class).addFilterEqual("department", instance);
+        // if(userService.count(userSearch) > 0) {
+        //     throw new OperationFailedException("Cannot delete department with assigned users.");
+        // }
+        return true;
+    }
+
+//    @Override
+    public Department saveInstance(Department department) throws ValidationFailedException, OperationFailedException {
+
+        Validate.notNull(department, "Department details are required");
+        Validate.hasText(department.getName(), "Department name is required");
+
+        // Ensure name is unique
+        Search search = new Search().addFilterEqual("name", department.getName());
+        if (department.isSaved()) {
+            search.addFilterNotEqual("id", department.getId());
+        }
+        if (departmentDao.count(search) > 0) {
+            throw new ValidationFailedException("A department with this name already exists.");
+        }
+
+        return super.save(department);
+    }
+}
