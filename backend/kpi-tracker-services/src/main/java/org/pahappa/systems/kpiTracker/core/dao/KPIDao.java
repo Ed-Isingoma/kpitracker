245dac0574b8a746d4a49ca909d6979c3faa4b77
genericodex <152326643+genericodex@users.noreply.github.com>
2025-08-15 12:17:42 +0300

    Added kpi daos

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/dao/KPIDao.java
@@ -0,0 +1,10 @@
+package org.pahappa.systems.kpiTracker.core.dao;
+
+import org.pahappa.systems.kpiTracker.models.KPI;
+import org.sers.webutils.server.core.dao.BaseDao;
+
+/**
+ * Data Access Object for the {@link KPI} entity.
+ */
+public interface KPIDao extends BaseDao<KPI> {
+}
