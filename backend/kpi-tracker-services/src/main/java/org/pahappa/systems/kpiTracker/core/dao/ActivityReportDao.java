9dc5dbc140402606e4cba58728e1bb087161358e
genericodex <152326643+genericodex@users.noreply.github.com>
2025-08-15 12:14:17 +0300

    Added new models

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/dao/ActivityReportDao.java
@@ -0,0 +1,10 @@
+package org.pahappa.systems.kpiTracker.core.dao;
+
+import org.pahappa.systems.kpiTracker.models.ActivityReport;
+import org.sers.webutils.server.core.dao.BaseDao;
+
+/**
+ * Data Access Object for the {@link ActivityReport} entity.
+ */
+public interface ActivityReportDao extends BaseDao<ActivityReport> {
+}
---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/dao/BusinessGoalDepartmentAssignmentDao.java
@@ -0,0 +1,10 @@
+package org.pahappa.systems.kpiTracker.core.dao;
+
+import org.pahappa.systems.kpiTracker.models.BusinessGoalDepartmentAssignment;
+import org.sers.webutils.server.core.dao.BaseDao;
+
+/**
+ * Data Access Object for the {@link BusinessGoalDepartmentAssignment} entity.
+ */
+public interface BusinessGoalDepartmentAssignmentDao extends BaseDao<BusinessGoalDepartmentAssignment> {
+}
