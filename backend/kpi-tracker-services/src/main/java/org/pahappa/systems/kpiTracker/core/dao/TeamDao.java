5761fb7e56e7a1557747cbe300eac115a6637a2e
genericodex <152326643+genericodex@users.noreply.github.com>
2025-08-15 12:17:00 +0300

    Added daos for team and peer rating

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/dao/TeamDao.java
@@ -0,0 +1,10 @@
+package org.pahappa.systems.kpiTracker.core.dao;
+
+import org.pahappa.systems.kpiTracker.models.Team;
+import org.sers.webutils.server.core.dao.BaseDao;
+
+/**
+ * Data Access Object for the {@link Team} entity.
+ */
+public interface TeamDao extends BaseDao<Team> {
+}
