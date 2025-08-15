d7a40773b0d6112efd1a411b8a5017a4aa3f87ec
genericodex <152326643+genericodex@users.noreply.github.com>
2025-08-15 13:27:54 +0300

    Added services for employees and teams

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/impl/TeamServiceImpl.java
@@ -0,0 +1,50 @@
+package org.pahappa.systems.kpiTracker.core.services.impl;
+
+import com.googlecode.genericdao.search.Search;
+import org.pahappa.systems.kpiTracker.core.dao.TeamDao;
+import org.pahappa.systems.kpiTracker.core.services.TeamService;
+import org.pahappa.systems.kpiTracker.core.services.impl.base.GenericServiceImpl;
+import org.pahappa.systems.kpiTracker.models.Team;
+import org.pahappa.systems.kpiTracker.utils.Validate;
+import org.sers.webutils.model.exception.OperationFailedException;
+import org.sers.webutils.model.exception.ValidationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("teamService")
+@Transactional
+public class TeamServiceImpl extends GenericServiceImpl<Team> implements TeamService {
+
+    @Autowired
+    private TeamDao teamDao;
+
+
+    @Override
+    public boolean isDeletable(Team instance) throws OperationFailedException {
+        // To be implemented: Check if team has users or KPIs.
+        return true;
+    }
+
+//    @Override
+    public Team saveInstance(Team team) throws ValidationFailedException, OperationFailedException {
+        Validate.notNull(team, "Team details are required");
+        Validate.hasText(team.getName(), "Team name is required");
+        Validate.notNull(team.getDepartment(), "Team must be assigned to a department");
+
+        // Ensure team name is unique within the department
+        Search search = new Search()
+                .addFilterEqual("name", team.getName())
+                .addFilterEqual("department", team.getDepartment());
+
+        if (team.isSaved()) {
+            search.addFilterNotEqual("id", team.getId());
+        }
+
+        if (teamDao.count(search) > 0) {
+            throw new ValidationFailedException("A team with this name already exists in this department.");
+        }
+
+        return this.teamDao.merge(team);
+    }
+}
